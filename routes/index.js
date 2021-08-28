2b8dc581f2af2dd84b03b96719e795135dddb6b3
monteromario <59968975+monteromario@users.noreply.github.com>
2021-08-28 11:48:28 +0200

    get_event

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,6 @@ const jwt = require('jsonwebtoken')
 
 const axios = require('axios').default;
 
-const rand = function() {
-    return Math.random().toString(36).substr(2);
-};
-
-const token = function() {
-    return rand() + rand() + rand() + rand();
-};
-
 
 router.get('/', (req, res, next) => {
   res.send('index');
@@ -31,118 +23,11 @@ router.get('/getEvents', (req, res, next) => {
   .catch((e) => {res.send(e)})
 });
 
-router.get('/users/me/:id', (req, res, next) => {
-  User.findById(req.params.id)
-    .then(user => {
-      if (!user) {
-        next(createError(404))
-      } else {
-        res.json(user)
-      }
-    })
-    .catch(err => {
-      res.json(err);
-    });
+router.get('/getEvent', (req, res, next) => {
+  axios.get("https://datos.madrid.es/egob/catalogo/tipo/evento/"+req.params.id+".json")
+  .then((r) => {res.send(r.data)})
+  .catch((e) => {res.send(e)})
 });
-
-router.post('/auth/login', (req, res, next) => {
-  const { email, password } = req.body
-
-  User.findOne({ email })
-    .then(user => {
-      if (!user) {
-        // Error if no user
-        res.json({errors: { message: 'Email or password is incorrect' }})
-        next(createError(404, { errors: { message: 'Email or password is incorrect' }}))
-      } else {
-        return user.checkPassword(password)
-          .then(match => {
-            if (!match) {
-              //Error if no password
-              res.json({errors: { message: 'Email or password is incorrect' }})
-              next(createError(404, { errors: { message: 'Email or password is incorrect' }}))
-            } else {
-              // JWT generation - only id is passed
-              res.json({ 
-                access_token: jwt.sign(
-                  { id: user._id },
-                  process.env.JWT_SECRET || 'changeme',
-                  {
-                    expiresIn: '1d'
-                  }
-                ),
-                userId: user._id
-               })
-            }
-          })
-      }
-    })
-})
-
-router.get('/users/delete/:id/:token', (req, res, next) => {
-  User.findOneAndRemove({_id: req.params.id, token: req.params.token})
-    .then(user => {
-      res.json(user)
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-router.post('/auth/singup', (req, res, next) => {
-  const { email, password, name, surname, picture, sex, city, age } = req.body;
-  User.create({
-    email: email,
-    password: password,
-    name: name, 
-    surname: surname, 
-    picture: picture,
-    sex: sex,
-    city: city,
-    age: age,
-    token: token(),
-    active: true
-  })
-    .then(response => {
-      res.json(response);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-})
-
-router.post('/auth/upload', (req, res, next) => {
-  
-})
-
-router.post('/users/edit/:id/:token', (req, res, next) => {
-  
-  const { email, name, surname, picture, sex, city, age } = req.body;
-
-  User.findOneAndUpdate({ _id: req.params.id, token: req.params.token}, {
-    email: email,
-    name: name, 
-    surname: surname, 
-    picture: picture,
-    sex: sex,
-    city: city,
-    age: age,
-  },)
-    .then(user => {
-      res.json(user)
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-router.post('/auth/logout', (req, res, next) => {
-  
-})
-
-router.get('/auth/loggedin', (req, res, next) => {
-  
-})
 
 
 module.exports = router;
